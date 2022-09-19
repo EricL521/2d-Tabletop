40b26e51b8e926ffbe740138348bd459084a16bc
GCreeper00 <liueric390@gmail.com>
2022-09-18 23:30:56 -0400

    set up server and socketio /api is now a proxy to localhost:3000

---
+++ server/app.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const path = require('path')
+const app = express();
+const port = 3000;
+const http = require('http');
+const server = http.createServer(app);
+
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+  res.send("api is online");
+});
+
+server.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+});
